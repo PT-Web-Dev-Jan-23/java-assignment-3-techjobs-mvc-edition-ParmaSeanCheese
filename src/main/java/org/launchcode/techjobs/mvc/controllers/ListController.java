cb6dae01e15924e7c58016ca7fa7e99952de946a
Sean <Seanpf12@hotmail.com>
2023-05-18 12:43:29 -0500

    Completed Project

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -29,6 +29,7 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
